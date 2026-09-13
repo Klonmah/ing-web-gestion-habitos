4fbe795f5fd8c9dbbc758ea9107ae3e6c024a224
Klonmah <klonmahh@gmail.com>
2026-09-13 04:06:48 -0300

    Inicio Proyecto Ionic+React

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ing-web-gestion-habitos',
+  webDir: 'dist'
+};
+
+export default config;
